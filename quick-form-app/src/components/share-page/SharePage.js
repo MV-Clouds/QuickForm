c6f955ec3b951f4125bc05c4ae506ca0a3341603
romitbhuptani <bhuptaniromit999@gmail.com>
2025-08-05 11:18:46 +0530

    Redirection to public link using svg

--- quick-form-app/src/components/share-page/SharePage.js
+++ quick-form-app/src/components/share-page/SharePage.js
@@ -122,8 +122,12 @@ const SharePage = () => {
             <div className="open-link">
               <button
                 className="open-link-button"
+                onClick={() => window.open(publishLink, '_blank', 'noopener,noreferrer')}
+                aria-label="Open public link"
+                type="button"
+                style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}
               >
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="redirect-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M13.3333 10.6667L20 4M20 4H15.5556M20 4V8.44444M20 13.7778V18.2222C20 18.6937 19.8127 19.1459 19.4793 19.4793C19.1459 19.8127 18.6937 20 18.2222 20H5.77778C5.30628 20 4.8541 19.8127 4.5207 19.4793C4.1873 19.1459 4 18.6937 4 18.2222V5.77778C4 5.30628 4.1873 4.8541 4.5207 4.5207C4.8541 4.1873 5.30628 4 5.77778 4H10.2222" stroke="#5F6165" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
               </button>
