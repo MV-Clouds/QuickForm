ad9f6c3916d573c7787bc2756c6205b31709cbe5
Vivek <vivekmiskin07@gmail.com>
2025-09-01 11:52:28 +0530

    Minor changes

--- quick-form-app/src/App.js
+++ quick-form-app/src/App.js
@@ -15,7 +15,7 @@ import GuestPageD from './components/LandingPage/GuestPageD';
 import NotificationPage from './components/NotificationSettings/NotificationSettingsModal';
 import NotFound from './components/not-found/NotFound';
 import SharePage from './components/share-page/SharePage'; // adjust path as necessary
-
+import LargeFileUpload from './components/file-upload/large-file-upload';
 function App() {
   return (
     // Set up the router for navigation
@@ -27,6 +27,7 @@ function App() {
             <Routes>
               {/* Public route for login */}
               <Route path="/" element={<Login />} />
+              <Route path='/upload' element ={<LargeFileUpload/>}/>
               <Route path="/guest" element={<ProtectedRoute element={< GuestPageD />} />} />
               {/* Protected route for form creation wizard */}
               <Route
